189792ff8e5a685258ccc5c8ac770d5cafe35fb1
deenakamal <deenah.kamal@gmail.com>
2023-10-15 13:55:09 -0700

    prinf file

--- _printf.c
+++ _printf.c
@@ -6,28 +6,30 @@
  */
 int _printf(const char *format, ...)
 {
-	specifier_match handlers[] = {
-		{"c", _print_char},
-		{"s", _print_string},
+	specifier_match h_list[] = {
+		{"c", _print_char}, {"s", _print_string},
 		{"%", _print_percent}
 	};
 	va_list arguments;
 	int num_spec = 2;
-	int i = 0;
-	int count = 0;
+	int i = 0, j = 0, count = 0;
 
 	va_start(arguments, format);
 
 	while (format[i] != '\0')
 	{
-		while (num_spec >= 0)
+		if (format[i] == '%')
 		{
-			if ((handlers[num_spec].specifier[i] == format[i]) && (handlers[num_spec].specifier[1] == format[i + 1]))
+			for (j = 0; j < sizeof(handlers) / sizeof(h_list[0]); j)
 			{
-				count = count + handlers[num_spec].f(arguments);
-				i = 1 + 2;
-			}
-			num_spec--;
+				if ((h_list[j].specifier[0] == format[i]) &&
+						(h_list[j].specifier[1] == format[i + 1]))
+				{
+					count += handlers[h_list].f(arguments);
+					i = 1 + 2;
+					break;
+				}
+				continue;
 		}
 		_putchar(format[i]);
 		count++;
