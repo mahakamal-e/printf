7bf02a7716ec842d54842f40bfa196aa74a680f3
deenakamal <deenah.kamal@gmail.com>
2023-10-15 14:49:20 -0700

    printf file

--- _printf.c
+++ _printf.c
@@ -8,31 +8,35 @@ int _printf(const char *format, ...)
 {
 	specifier_match h_list[] = {
 		{"c", _print_char}, {"s", _print_string},
-		{"%", _print_percent}
+		{"%%", _print_percent}
 	};
 	va_list arguments;
-	int num_spec = 2;
 	int i = 0, j = 0, count = 0;
 
 	va_start(arguments, format);
+	if ((format[0] == '%' && format[1] == '\0') || format == NULL)
+		return (-1);
 
 	while (format[i] != '\0')
 	{
 		if (format[i] == '%')
 		{
-			for (j = 0; j < sizeof(handlers) / sizeof(h_list[0]); j)
+			for (j = 0; j < (int)(sizeof(h_list) / sizeof(h_list[0])); j++)
 			{
 				if ((h_list[j].specifier[0] == format[i]) &&
 						(h_list[j].specifier[1] == format[i + 1]))
 				{
-					count += handlers[h_list].f(arguments);
-					i = 1 + 2;
+					count += h_list[j].f(arguments);
+					i = i + 2;
 					break;
 				}
-				continue;
+			}
 		}
+		else
+		{
 		_putchar(format[i]);
 		count++;
+		}
 		i++;
 	}
 	va_end(arguments);
